ead3c5693799c2058db75cb640cef9555557a801
Caroline Andrade <andradecostacaroline@gmail.com>
2023-07-17 18:01:55 -0300

    master: alterando os dados do JSON

--- Aula05/Pessoa.py
+++ Aula05/Pessoa.py
@@ -2,7 +2,7 @@
 
 import json
 
-json_data = '{"nome": "Amanda", "idade": 28, "cidade": "Joao Pessoa"}'
+json_data = '{"nome": "Caroline", "idade": 31, "cidade": "Campina Grande"}'
 data = json.loads(json_data)
 
 print(data["nome"])
